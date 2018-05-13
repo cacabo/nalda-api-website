8e81e62fdad0d05dc9071772e1467ea6d3ba10a7
ccabo1 <ccabo@seas.upenn.edu>
2018-05-13 14:19:48 -0700

    Video routes

--- routes.js
+++ routes.js
@@ -2,9 +2,19 @@
 const express = require('express');
 const router = express.Router();
 
+// Import data
+const videos = require('./src/json/videos');
+
 // Homepage
 router.get('/', (req, res) => res.render('home', {
   title: 'Nalda API',
+  videos,
+}));
+
+// Videos
+router.get('/videos', (req, res) => res.render('videos', {
+  title: 'Nalda API | Videos',
+  videos,
 }));
 
 // Handle 404 error
