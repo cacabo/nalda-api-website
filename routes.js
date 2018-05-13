d28ab4972d1c05ea21a9d7621539b09aff9a8960
ccabo1 <ccabo@seas.upenn.edu>
2018-05-13 16:43:07 -0700

    Rendering articles, arrays

--- routes.js
+++ routes.js
@@ -4,9 +4,15 @@ const router = express.Router();
 
 // Import data
 const rawVideos = require('./src/json/videos');
+const rawArticles = require('./src/json/articles');
+console.log(rawArticles[3].params.body);
 const videos = rawVideos.map(video => {
   video.params = toString(video.params);
   return video;
+});
+const articles = rawArticles.map(article => {
+  article.params = toString(article.params);
+  return article;
 });
 
 // Helper function
@@ -18,6 +24,18 @@ function toString(thing, depth) {
   }
 
   if (typeof thing === 'object') {
+    // If this is an array (which, in JS, is a type of object)
+    if (thing.constructor === Array) {
+      let string = '[<br />';
+      let value;
+      for (value in thing) {
+        string += `${indent}&nbsp;&nbsp;${toString(thing[value], depth ? (depth + 1) : 1)},<br />`;
+      }
+      string += `${indent}]`;
+      return string;
+    }
+
+    // If this is a normal object
     let string = '{<br/>';
     let key;
     for (key in thing) {
@@ -33,6 +51,7 @@ function toString(thing, depth) {
 router.get('/', (req, res) => res.render('home', {
   title: 'Nalda API',
   videos,
+  articles,
 }));
 
 // Get a specific route
@@ -47,10 +66,28 @@ router.get('/routes/*', (req, res) => {
   let route = null;
 
   // Find which object the route is in
-  if (path.startsWith('/videos')) {
+  if (path.startsWith('/api/videos')) {
     videos.forEach(video => {
       if (video.route === path) {
         route = video;
+      }
+    });
+    if (route) {
+      res.render('route', {
+        title: `Nalda API | ${path}`,
+        route,
+      });
+      return;
+    }
+
+    // If no matching route was found
+    res.render('not-found', {
+      title: 'Nalda API | Not Found',
+    });
+  } else if (path.startsWith('/api/articles')) {
+    articles.forEach(article => {
+      if (article.route === path) {
+        route = article;
       }
     });
     if (route) {
@@ -76,6 +113,12 @@ router.get('/routes/*', (req, res) => {
 router.get('/videos', (req, res) => res.render('videos', {
   title: 'Nalda API | Videos',
   videos,
+}));
+
+// Articles
+router.get('/articles', (req, res) => res.render('articles', {
+  title: 'Nalda API | Articles',
+  articles,
 }));
 
 // Handle 404 error
